d6adf9560488e0d1cd5a514d5f5bfd1bd6549cf3
danlogan725 <dl.logan.dan@gmail.com>
2026-05-18 21:47:12 +0100

    End of Monday

--- Plotting_Dan.py
+++ Plotting_Dan.py
@@ -53,6 +53,34 @@ def plot_PSTH(spikes, Ntrials=10, binsize=10):
     plt.show()
 
 
+def fano_factor(spikes, Ntrials=10, binsize=10):
+
+    #calculate Fano factor to understand variability in spike counts across trials
+
+    #find loactions of spikes
+    #iterate through bins and trials
+    # cout number of spikes in each bin for each trial
+    # calculate mean and variance of spike counts across trials for each bin
+    #Fano factor = variance / mean
+
+
+    #list of all spikes reqired for finding max time for binning
+    all_spike_times = []
+    for trial in range(Ntrials):
+        trial_spikes = np.where(spikes[trial] > 0)[0]
+        all_spike_times.extend(trial_spikes)
+
+    # define bins from 0 to max spike time with specified binsize
+    bins = np.arange(0, max(all_spike_times) + binsize, binsize)
+
+    spike_counts = np.zeros((Ntrials, len(bins) - 1))
+
+    for trial in range(Ntrials):
+        trial_spikes = np.where(spikes[trial] > 0)[0]
+        spike_counts[trial], _ = np.histogram(trial_spikes, bins=bins)
+    mean_counts = np.mean(spike_counts, axis=0)
+    var_counts = np.var(spike_counts, axis=0)
+    fano_factors = var_counts / mean_counts
 
 
 
